f9646656ec3408f1591222462c8769d3990b05ab
kimmido <icecream120102@gmail.com>
2024-07-16 16:43:49 +0900

    프로젝트 시작

---
+++ src/components/Gnb.js
@@ -0,0 +1,15 @@
+
+function Gnb() {
+
+  return (
+      <div className='gnb'>
+        <div className='container'>
+          <div className='list'></div>
+          <div className='home'></div>
+          <div className='my'></div>
+        </div>
+      </div>
+  );
+}
+
+export default Gnb;
