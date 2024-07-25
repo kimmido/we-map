c5f109740f102200943d3cdf8755ed2672c014b8
kimmido <icecream120102@gmail.com>
2024-07-25 10:35:33 +0900

    검색 장소로 좌표 이동 완료

--- src/pages/Place.js
+++ src/pages/Place.js
@@ -1,27 +1,29 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Map from '../components/Map';
 import '../assets/style/pages/Place.css'
 
 const { kakao } = window;
 
-const Place = (props) => {
+const Place = () => {
     // const { goalPosition, setGoalPosition } = props;
-    const searchPlaceInfo = useLocation().state;
+    const searchPlaceInfo = useRef();
+    searchPlaceInfo.current = useLocation().state;
     const [ placePosition, setPlacePosition ] = useState(null);
-    // console.log(searchPlaceInfo);'
-    // useEffect(()=> {
-    //     gotoPlacePosition(searchPlaceInfo);
-    // }, [])
 
-    const gotoPlacePosition = useCallback((place)=> {
-        if(place) {
-            const pos = new kakao.maps.LatLng(place.lat, place.lng);
-            console.log(place);
-            // setPlacePosition(pos);
-            console.log("아");
-            // setGoalPosition(locPosition);
-        }
+    console.log(searchPlaceInfo.current);
+
+    useEffect(()=> {
+        goToPlacePosition(searchPlaceInfo.current);
+    }, [])
+
+    const goToPlacePosition = useCallback((place)=> {
+        const pos = new kakao.maps.LatLng(place.lat, place.lng);
+        setPlacePosition(pos);
+
+        console.log(place);
+        console.log(pos);
+        console.log("goToPlacePosition");
     }, [])
 
     
