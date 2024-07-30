1ba55176573b4700ac9045132ab7673a2bc9b79c
kimmido <icecream120102@gmail.com>
2024-07-30 14:45:50 +0900

    e를 찾을 수 없다는 에러 발생

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -16,16 +16,15 @@ function Home(props) {
     }, [])
 
     const goToUserPosition = useCallback(() => {
-        console.log("home.js callback");
         if (navigator.geolocation) { // geolocation을 지원한다면
         navigator.geolocation.getCurrentPosition(
-            (pos) => { // 위치 찾기 성공{위도, 경도}
-            const lat = pos.coords.latitude;
-            const lng = pos.coords.longitude;
+            (pos) => {
+                const lat = pos.coords.latitude;
+                const lng = pos.coords.longitude;
 
-            const locPosition = new kakao.maps.LatLng(lat, lng);
-            
-            setUserPosition(locPosition);
+                const locPosition = new kakao.maps.LatLng(lat, lng);
+                
+                setUserPosition(locPosition);
             }, 
             (error) => {errorLocation(error)})
         }else{
@@ -36,22 +35,23 @@ function Home(props) {
     const errorLocation = useCallback((e) => {
         let print;
         switch(e.code) {
-        case e.PERMISSION_DENIED: // 1
-        print = "이 문장은 사용자가 Geolocation API의 사용 요청을 거부했을 때 나타납니다!"
-        break;
+            case e.PERMISSION_DENIED: // 1
+            print = "이 문장은 사용자가 Geolocation API의 사용 요청을 거부했을 때 나타납니다!"
+            break;
 
-        case e.POSITION_UNAVAILABLE: // 2
-        print = "이 문장은 가져온 위치 정보를 사용할 수 없을 때 나타납니다!"
-        break;
+            case e.POSITION_UNAVAILABLE: // 2
+            print = "이 문장은 가져온 위치 정보를 사용할 수 없을 때 나타납니다!"
+            break;
 
-        case e.TIMEOUT: // 3
-        print = "이 문장은 위치 정보를 가져오기 위한 요청이 허용 시간을 초과했을 때 나타납니다!"
-        break;
+            case e.TIMEOUT: // 3
+            print = "이 문장은 위치 정보를 가져오기 위한 요청이 허용 시간을 초과했을 때 나타납니다!"
+            break;
 
-        case e.UNKNOWN_ERROR:
-        print = "이 문장은 알 수 없는 오류가 발생했을 때 나타납니다!"
-        break;
+            case e.UNKNOWN_ERROR:
+            print = "이 문장은 알 수 없는 오류가 발생했을 때 나타납니다!"
+            break;
         }
+        alert(print);
     }, []);
 
     return (
