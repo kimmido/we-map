969ac452a0b49154f50debb737271ccfada97664
kimmido <icecream120102@gmail.com>
2024-08-02 10:02:50 +0900

    Updates

---
+++ static/js/952.925e85f7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwe_map=self.webpackChunkwe_map||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.925e85f7.chunk.js.map
