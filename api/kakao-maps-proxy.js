d5479176ee1d16730fc234fc5c20321aac19e20c
kimmido <icecream120102@gmail.com>
2024-08-14 12:45:30 +0900

    HashRouter로 변경

--- api/kakao-maps-proxy.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-    const kakaoMapUrl = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY}&libraries=services`;
-  
-    res.redirect(kakaoMapUrl);
-  }
