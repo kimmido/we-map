0b643730e495dedde65cd739617d39c7415617a2
kimmido <icecream120102@gmail.com>
2024-09-05 17:28:41 +0900

    prettier 적용

--- src/pages/Place.js
+++ src/pages/Place.js
@@ -1,54 +1,49 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import Map from '../components/Map';
-import '../assets/style/pages/Place.css'
-import PlacePopup from './PlacePopup';
-import SearchBar from '../components/SearchBar';
-import { getCurrentPlaceReviews } from '../utils/supabaseFatch';
+import React, { useCallback, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import Map from "../components/Map";
+import "../assets/style/pages/Place.css";
+import PlacePopup from "./PlacePopup";
+import SearchBar from "../components/SearchBar";
+import { getCurrentPlaceReviews } from "../utils/supabaseFatch";
 
 const { kakao } = window;
 
 const Place = ({ user, userLists }) => {
-    const [ placePosition, setPlacePosition ] = useState(null);
-    const [ reviews, setReviews] = useState([]);
-    const { id, lat, lng, name, phone, address } = useLocation().state;
-    
-    useEffect(()=> {
-        goToPlacePosition(lat, lng);        
-    
-        const listIdArr = userLists.map(list => list.list_id);
-        
-        getCurrentPlaceReviews(id, listIdArr)
-            .then(data => {
-                setReviews(data);
-            }).catch((error) => {
-                console.log(error);
-            })
+  const [placePosition, setPlacePosition] = useState(null);
+  const [reviews, setReviews] = useState([]);
+  const { id, lat, lng, name, phone, address } = useLocation().state;
 
-        return () => {
-        };
-    }, [userLists])
-    
+  useEffect(() => {
+    const listIdArr = userLists.map((list) => list.list_id);
 
-    const goToPlacePosition = useCallback((lat, lng)=> {
-        const pos = new kakao.maps.LatLng(lat, lng);
-        setPlacePosition(pos);
-    }, [])
+    getCurrentPlaceReviews(id, listIdArr)
+      .then((data) => {
+        setReviews(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [userLists]);
 
-    return (
-        <div className='Place'>
-            <SearchBar
-                displayText={name} />
-            <Map goalPosition={placePosition} />
-            <PlacePopup
-                user={user}
-                id={id}
-                reviews={reviews}
-                name={name}
-                address={address}
-                phone={phone} />
-        </ div>
-    );
-}
+  const goToPlacePosition = useEffect(() => {
+    const pos = new kakao.maps.LatLng(lat, lng);
+    setPlacePosition(pos);
+  }, []);
+
+  return (
+    <div className="Place">
+      <SearchBar displayText={name} />
+      <Map goalPosition={placePosition} />
+      <PlacePopup
+        user={user}
+        id={id}
+        reviews={reviews}
+        name={name}
+        address={address}
+        phone={phone}
+      />
+    </div>
+  );
+};
 
 export default Place;
